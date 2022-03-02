f64e7a5c668a9f0f89a76d7e82f26bd14e7fd7fd
Scriper.irk <scriper.irk@gmail.com>
2022-03-03 00:13:26 +0800

    initial commit

--- src/test/java/ru/netology/SQRService/CalculTest.java
+++ src/test/java/ru/netology/SQRService/CalculTest.java
@@ -2,6 +2,8 @@ package ru.netology.SQRService;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CalculTest {
     //все попадают в диапазон
     @Test
@@ -9,9 +11,12 @@ class CalculTest {
         Calcul service = new Calcul();
         int max = 300;
         int min = 200;
+        int expected = 3;
 
         int actual = service.sqrtcalc(max, min);
         System.out.println(actual);
+
+        assertEquals(expected,actual);
     }
 
     //ничего не попадает в диапазон
@@ -20,9 +25,12 @@ class CalculTest {
         Calcul service = new Calcul();
         int max = 90;
         int min = 50;
+        int expected = 0;
 
         int actual = service.sqrtcalc(max, min);
         System.out.println(actual);
+
+        assertEquals(expected,actual);
     }
 
     //один попадает в диапазон
@@ -31,8 +39,10 @@ class CalculTest {
         Calcul service = new Calcul();
         int max = 100;
         int min = 50;
+        int expected = 1;
 
         int actual = service.sqrtcalc(max, min);
         System.out.println(actual);
+        assertEquals(expected,actual);
     }
 }
